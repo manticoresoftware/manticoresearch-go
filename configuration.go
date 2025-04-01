c75341ffb5994474593186573888a244aa315c34
nick <nick@manticoresearch.com>
2025-04-01 14:08:27 +0700

    Generated the release version

--- configuration.go
+++ configuration.go
@@ -87,7 +87,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/1.7.0/go",
+		UserAgent:        "OpenAPI-Generator/1.8.0/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
