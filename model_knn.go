13ed791c114647b428f16954c13ee7092d813339
nick80 <nick@manticoresearch.com>
2026-02-12 14:16:15 +0700

    Regenerated client

--- model_knn.go
+++ model_knn.go
@@ -174,9 +174,9 @@ func (o *Knn) SetQueryVector(v []float32) {
 }
 
 // GetDocId returns the DocId field value if set, zero value otherwise.
-func (o *Knn) GetDocId() int32 {
+func (o *Knn) GetDocId() uint64 {
 	if o == nil || IsNil(o.DocId) {
-		var ret int32
+		var ret uint64
 		return ret
 	}
 	return *o.DocId
@@ -184,7 +184,7 @@ func (o *Knn) GetDocId() int32 {
 
 // GetDocIdOk returns a tuple with the DocId field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *Knn) GetDocIdOk() (*int32, bool) {
+func (o *Knn) GetDocIdOk() (*uint64, bool) {
 	if o == nil || IsNil(o.DocId) {
 		return nil, false
 	}
@@ -201,7 +201,7 @@ func (o *Knn) HasDocId() bool {
 }
 
 // SetDocId gets a reference to the given int32 and assigns it to the DocId field.
-func (o *Knn) SetDocId(v int32) {
+func (o *Knn) SetDocId(v uint64) {
 	o.DocId = &v
 }
 
